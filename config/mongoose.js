012a49860b3d73f9dad869bf9cee282991a23833
shivam <geetapandey595@gmail.com>
2020-08-27 03:52:56 +0530

    cloud connection added to mongodb_atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,42 @@
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/todo_list_db');
+
+// const db = mongoose.connection;
+// db.on('error', console.log.bind(console, 'error connecting to db'));
+
+// db.once('open', function(){
+//     console.log("successfully connected to database");
+// });
+
+//****************************NEW CONNECTION TO CONNECT TO CLOUD BASED DATABASE--**************************************
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo_list_db');
+const {mongoClient} = require('mongodb')
+
+//const uri = "mongodb+srv://uchihabloodline:YDRYCt9TqNJeQB3a@cluster0-lx13z.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://uchihabloodline:YDRYCt9TqNJeQB3a@cluster0.lx13z.mongodb.net/Ecommerce?retryWrites=true&w=majority";
+// const pass = "YDRYCt9TqNJeQB3a";
+// const dbuser = "uchihabloodine";
+// const dbname = "Ecommerce";
+
+mongoose.connect(uri,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on('error', console.log.bind(console, 'error connecting to db'));
+db.on('connected', () => {
+    console.log('Mongoose connected to db...');
+  });
+
+db.on('error', console.error.bind(console,"error connecting to DB!!"));
+
+// db.on('disconnected', () => {
+//     console.log('Mongoose connection is disconnected...');
+//   })
 
 db.once('open', function(){
-    console.log("successfully connected to database");
-});
+    console.log('Successfully connected to Atlas DB!');
+})
+
+ module.exports = db;
 
--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Card = require('./models/card');
 // Middleware
 app.set('view engine', 'ejs');
 app.set('views', './views');
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('assets'));
 
 app.get('/', function(req, res){
